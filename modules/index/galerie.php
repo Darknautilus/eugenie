e6088596f614c97190c08417be9f74c535e94075
Darknautilus <aurelienbertron@gmail.com>
2013-03-02 17:48:26 +0100

    Ajoute l'animation et le chargement asynchrone

--- modules/index/galerie.php
+++ modules/index/galerie.php
@@ -1,3 +1,7 @@
 <?php
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']))
+  $async = true;
+else
+  $async = false;
 
-echo $twig->render("index_galerie.html", array());
+echo $twig->render("index_galerie.html", array("async" => $async));
--- modules/index/thes.php
+++ modules/index/thes.php
@@ -1,3 +1,8 @@
 <?php
 
-echo $twig->render("index_thes.html", array());
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']))
+  $async = true;
+else
+  $async = false;
+
+echo $twig->render("index_thes.html", array("async" => $async));
