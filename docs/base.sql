19f07e1f5325eced0694a40c6d5f2fb8cebfc07e
tbille <thomas.bille625@gmail.com>
2013-05-13 19:02:48 +0100

    Met a jour la base sql

--- docs/base.sql
+++ docs/base.sql
@@ -1,10 +1,18 @@
 DROP TABLE IF EXISTS LivreOr;
-
+DROP TABLE IF EXISTS administrateur;
 
 CREATE TABLE LivreOr (
 	idComm	int AUTO_INCREMENT,
 	commentaire	varchar(250),
-	valide	boolean,
-	CONSTRAINT pk_type PRIMARY KEY (idType)
+	valide	int,
+	CONSTRAINT pk_livreOr PRIMARY KEY (idComm)
 )ENGINE=InnoDB CHARSET=UTF8;
 
+CREATE TABLE administrateur (
+	idAdmin	int AUTO_INCREMENT,
+	identifiant	varchar(30),
+	mdp	varchar(250),
+	CONSTRAINT pk_admin PRIMARY KEY (idAdmin)
+)ENGINE=InnoDB CHARSET=UTF8;
+
+INSERT INTO `administrateur`(`identifiant`, `mdp`) VALUES ('salonEugenie','416b724d615048595a6d4d255674483230613f44316c6e6e385271384875262abc26e3c1ecac246487fc548f5141d910b93b1a002b87e707c5024dae0b6a2d97');
