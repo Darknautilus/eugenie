ca91dbaa4e954a0abce8a41b9c5a1769c4d0228b
Darknautilus <aurelienbertron@gmail.com>
2013-05-14 19:37:33 +0200

    repair .gitignore

--- docs/base.sql
+++ docs/base.sql
@@ -16,3 +16,4 @@ CREATE TABLE administrateur (
 )ENGINE=InnoDB CHARSET=UTF8;
 
 INSERT INTO `administrateur`(`identifiant`, `mdp`) VALUES ('salonEugenie','416b724d615048595a6d4d255674483230613f44316c6e6e385271384875262abc26e3c1ecac246487fc548f5141d910b93b1a002b87e707c5024dae0b6a2d97');
+INSERT INTO `LivreOr` (`idComm`, `commentaire`, `valide`) VALUES (1, 'Très bon poulet', 0);
